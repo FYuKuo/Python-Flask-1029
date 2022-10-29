77cc74455fa42d89bf7fe7ec1a84c4ee5e6ae27b
FY Kuo <teresa99339@gmail.com>
2022-10-29 19:21:21 +0800

    form表單 傳遞資料成功

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
@@ -8,7 +8,25 @@ def index():
 
 
 @app.route("/page/text")
-def text():
+def pageText():
     return render_template('page.html', text="Python Flask!")
+
+
+@app.route("/form")
+def formPage():
+    return render_template('form.html')
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+
+    print("name",name)
+    print("email",email)
+    return redirect(url_for('success', name=name , action = "post"))
+
+@app.route("/success/<action>/<name>")
+def success(action,name):
+    return  f"{action} : welcome {name}"
 
 app.run()
