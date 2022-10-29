b5db1b98431de2faaaa3dd00d99bb7b3acf15040
FY Kuo <teresa99339@gmail.com>
2022-10-29 13:42:55 +0800

    測試templates成功

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Flask"
+    return render_template('home.html')
+
+
 
 app.run()
